32ce83646741f33a6daa7268a218b79e5f60c880
Ryan Kelley <rskelley9@gmail.com>
2014-01-27 23:34:07 -0500

    set-up rails with pg and bootstraps and base theme

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PersonalSite::Application.config.secret_key_base = '56c3da431a4436b8145916126a91855100aa62c3775033692db0bb256ca5249578e37ca1a827887c3c45066d1cce3558cf399d0aa69a23761f725fa6cc3916c3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PersonalSite::Application.config.session_store :cookie_store, key: '_Personal_Site_session'
